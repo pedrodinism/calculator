e54fd20ccf7ac35500fcb12a9081596a23f976ba
Pedro Marques <52702280+pedrodinism@users.noreply.github.com>
2025-11-18 16:00:12 +0100

    added the functions: display, cleanVars and the event listener

--- script.js
+++ script.js
@@ -14,27 +14,69 @@ function multiply (a,b) {
     return a * b
 }
 
-let number1 = 0
-let number2 = 0
+let number1 = ""
+let number2 = ""
 let operator = ""
+let result = ""
+const operators = ["/", "x", "-", "+"]
 
 function calculate (operator, number1, number2) {
+    number1 = Number(number1)
+    number2 = Number(number2)
     switch (operator) {
         case "+": 
-            add(number1, number2)
-            console.log("called add")
-            return
+            return add(number1, number2)
         case "-":
-            subtract(number1, number2)
-            console.log("called subtract")
-            return
+            return subtract(number1, number2)
         case "/":
-            divide(number1, number2)
-            console.log("called divide")
-            return 
-        case "*":
-            multiply(number1, number2)
-            console.log("called multiply")
-            return        
+            return divide(number1, number2)
+        case "x":
+            return multiply(number1, number2)    
     }
 }
+
+function display (value) {
+    document.querySelector('.display').textContent = value
+}
+
+function cleanVars () {
+    number1 = ""
+    number2 = ""
+    operator = ""
+    display("")
+}
+
+
+document.querySelector('.buttons').addEventListener("click", (event) => {
+    let value = document.getElementById(event.target.id).textContent
+    if (value === "C") {
+        cleanVars()
+        return
+    }
+    if (operators.includes(value)) {
+        operator = value
+        display(operator)
+        return
+    }
+    if (Number.isInteger(Number(value)) && operator === "") {
+        number1 = number1 + value
+        display(number1)
+        return
+    }
+    if (Number.isInteger(Number(value)) && operator != "") {
+        number2 = number2 + value
+        display(number2)
+        return
+    }
+    if (value === "=") {
+        result = calculate(operator, number1, number2)
+        display(result)
+        return
+    }
+    if (result != 0) {
+        cleanVars()
+        number1 = value
+        display(number1)
+    } 
+})
+
